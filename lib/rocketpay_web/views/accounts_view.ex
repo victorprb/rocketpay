0fcac5975abd52941a71ca5c424885a910f0c6e4
victorprb <victorp.ribeiro@gmail.com>
2021-02-25 15:27:26 -0300

    Add withdraw and transaction operations

--- lib/rocketpay_web/views/accounts_view.ex
+++ lib/rocketpay_web/views/accounts_view.ex
@@ -1,12 +1,29 @@
 defmodule RocketpayWeb.AccountsView do
   alias Rocketpay.Account
+  alias Rocketpay.Accounts.Transactions.Response, as: TransactionResponse
 
   def render("update.json", %{account: %Account{id: id, balance: balance}}) do
     %{
-      message: "Account updated",
+      message: "Balance updated",
       account: %{
         id: id,
         balance: balance
+      }
+    }
+  end
+
+  def render("transaction.json", %{transaction: %TransactionResponse{to_account: to_account, from_account: from_account}}) do
+    %{
+      message: "Transaction done succesfully",
+      transaction: %{
+        from_account: %{
+          id: from_account.id,
+          balance: from_account.balance
+        },
+        to_account: %{
+          id: to_account.id,
+          balance: to_account.balance
+        }
       }
     }
   end
